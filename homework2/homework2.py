53700d8e745fb2a9625f7d6db8c042885cfefaf7
jofro9 <jfroeli9@gmail.com>
2021-06-11 17:19:50 -0600

    homework 1 stuff

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import numpy as np
+def correlation ( attribute1 , fileName1 , attribute2, fileName2 ):
+    '''
+    Input Parameters:
+        attribute1: The attribute you want to consider from file1
+        attribute2: The attribute you want to consider from file2
+        fileName1: The comma seperated file1 with the different format first line removed
+        fileName2: The comma seperated file2 with the different format first line removed
+
+    Output:
+        Return the correlation coefficient
+        Do not return a string
+        Do not write anything to stdout
+        Points will be deducted if these instructions are not followed strictly
+
+    '''
+    #TODO: Write code given the Input / Output Paramters.
+
 
